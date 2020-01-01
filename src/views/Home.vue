5d5c0d42d8d0a3e98f7d5eb55460a24e2bc592d9
zhangwenshun <vendy9229@163.com>
2020-01-01 20:39:28 +0800

    modify

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,8 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <Test></Test>
+    <Test msg="test111"></Test>
+    <button @click="ontest">test throw</button>
   </div>
 </template>
 
@@ -15,6 +16,13 @@ export default {
   name: 'home',
   components: {
     Test
+  },
+  methods: {
+    ontest () {
+      throw new TypeError('eeee')
+    }
   }
 }
+// var a = 1
+// b = c
 </script>
